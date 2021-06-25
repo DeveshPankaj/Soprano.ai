1c0ce8c2f1f8e40702e963e3251349af2a137b23
PANKAJ DEVESH <pankajdevesh3@gmail.com>
2021-06-25 23:05:40 +0530

    Removed Commented code

--- api/src/db.ts
+++ api/src/db.ts
@@ -1,35 +1,21 @@
 import {MongoClient} from 'mongodb'
 import * as mongoose from 'mongoose'
 import userModel from './models/user.model'
-const myurl = 'mongodb://mongo:27017/soprano';
+
+// TODO: Database Configuration should come from .env
+const myurl = 'mongodb://mongo:27017/soprano'
 
 // MongoClient.connect(myurl, (err, client) => {
 //   if (err) return console.log(err)
-
 //   console.log('Connected!')
-  
 //   const db = client.db('admin')
 //   const collection = db.collection('user')
 //   collection.findOne({}, (error, result) => {
 //       console.log(result)
 //   })
-  
 // })
 
 
-// async function run(): Promise<void> {
-//     let conn = mongoose.connect(myurl, { useNewUrlParser: true});
-
-//     // let userModel = conn.model('user', userSchema)
-//     conn.then(()=>{
-
-//     })
-//     userModel.findOne({}, (error, data) => {
-//         console.log(error, data)
-//     })
-// }
-
-// run()
 
 const options = {
     useNewUrlParser: true,
@@ -45,17 +31,6 @@ const options = {
 //@ts-ignore
 mongoose.connect(myurl, options as any).then(() => {
     console.log("MongoDB connection is Connected");
-
-// userModel.create({ first_name: 'udgshdfgh',
-//     last_name: '0',
-//     email: ';',
-//     password: 'string', avatar:'husdfu'})
-
-// userModel.find().then(result => {
-//     console.log(result)
-// })
-
-
   })
   .catch(err => {
     console.log("MongoDB connection is Down.Error is...", err);
